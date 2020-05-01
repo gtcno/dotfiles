33b7c57fdf56b705d1b4d153433b0d286798b8d8
Giao The Cung <giaothe@gmail.com>
2020-05-01 08:14:31 +0200

    Changes.

--- kubernetes/config.zsh
+++ kubernetes/config.zsh
@@ -1,1 +1,2 @@
 export KUBECONFIG=$HOME/.kube/config:/Users/giaocung/dev/sandboxes/kubeconfigs/config
+export PATH="${PATH}:${HOME}/.krew/bin"
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,3 +3,5 @@ alias reload!='. ~/.zshrc'
 alias cls='clear' # Good 'ol Clear Screen command
 alias jumphost='ssh C150660@a01apvl00057.adeo.no'
 alias gw='gradle'
+alias gwb='gradle build'
+alias gws='gradle spotlessApply'
