0b2b42def65f536daa7f49df6d620a5ddebe0f58
Giao The Cung <giaothe@gmail.com>
2022-09-05 08:08:48 +0200

    Stuff

--- kubernetes/config.zsh
+++ kubernetes/config.zsh
@@ -1,2 +1,3 @@
 export KUBECONFIG=/Users/giaocung/dev/sandboxes/kubeconfigs/config
 export KUBE_EDITOR="vim"
+export USE_GKE_GCLOUD_AUTH_PLUGIN=True
--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,1 +1,1 @@
-export PATH="$HOME/bin:$PATH":/Users/giaocung/dev/sandboxes/dagpenger-iac/postgres-gcp
+export PATH="$HOME/bin:$PATH":/Users/giaocung/dev/sandboxes/dagpenger-iac/postgres-gcp:/Users/giaocung/.krew/bin:/opt/homebrew/Cellar/libpq/14.2/bin
